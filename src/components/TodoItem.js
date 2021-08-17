4035f920b3d3e867353b3e0835bccd40a1f97b69
Jason Vinson <vinsonizer@gmail.com>
2021-08-17 07:21:41 -0400

    Added editing

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,6 +2,19 @@ import React from "react"
 import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
+
+  state = {
+    editing: false
+  }
+
+  handleEditing = () => {
+    this.setState({editing: true})
+  }
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({editing: false})
+    }
+  }
   render() {
     const completedStyle = {
       fontStyle: "italic",
@@ -10,17 +23,39 @@ class TodoItem extends React.Component {
       textDecoration: "line-through"
     }
     const {completed, id, title} = this.props.todo
-    return (<li className={styles.item}>
-      <input type="checkbox" className={styles.checkbox} checked={completed} onChange={() => this.props.handleChangeProps(id)}/>
-      <button onClick={() => this.props.deleteTodoProps(id)}>
-        Delete
-      </button>
-      <span style={completed
-          ? completedStyle
-          : null}>
-        {title}
-      </span>
-    </li>)
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
+    return (
+      <li className={styles.item}>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)} />
+          <button onClick={() => this.props.deleteTodoProps(id)}> Delete </button>
+          <span style={ completed ? completedStyle : null }>
+            {title}
+          </span>
+        </div>
+        <input type="text"
+          className={styles.textInput}
+          style={editMode}
+          value={title}
+          onChange={ e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown = {
+            this.handleUpdatedDone
+          }/>
+      </li>
+    )
   }
 }
 
