8c8890c33e04a89f1a44d767c9bab4b74b4f18b0
Jason Vinson <vinsonizer@gmail.com>
2021-08-18 16:11:47 -0400

    Conversion to functions instead of components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
-import "./App.css"
+import TodoContainer from "./functionBased/components/TodoContainer"
+import "./functionBased/App.css"
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode >
     <TodoContainer/>
   </React.StrictMode>,
-    document.getElementById("root")
- )
+  document.getElementById("root")
+)
